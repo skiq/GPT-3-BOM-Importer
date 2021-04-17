78aa02030d396c7f92853a96b0d4412ae92c0d7b
Swen Koller <swenkoller@Swens-MacBook-Pro.local>
2021-04-17 22:50:35 +0200

    return csv path

--- results.py
+++ results.py
@@ -89,6 +89,7 @@ def append_excel_with_mpns_get_csv(original_excel,mpn_candidates):
 def get_result_csv(original_excel_file,mpns):
     remove_invalid_mpns(mpns)
     append_excel_with_mpns_get_csv(original_excel_file,mpns)
+    return original_excel_file.strip(".xls")[0] + ".csv"
 
 """
 takes list as input and deletes all invalid MPNs
@@ -132,11 +133,12 @@ if __name__ == '__main__':
     mpns = [[1, 'Qty.|Value|Package|Parts|Producer|Producer Number|Description|Dist'], [3, 'C0603C105K3RACTU'], [5, 'GRM188R72A104KA35D'], [7, 'MC0603B102K500CT'], [9, '0603B103J500CT'], [11, 'C1608X5R1E106M080AC'], [13, '0603B103J500CT'], [15, '#|#|'], [17, 'C9'], [19, '0603B472K500CT'], [21, 'GRM31CR71A226ME15L'], [23, ''], [25, 'C1206C475K5P'], [27, 'C3216X7T2E224M160AA'], [29, 'CD0603_S01575'], [31, 'SMAJ18CA'], [33, '634-SI8261BAC-C-IS'], [35, 'CD4093BPWR'], [37, ''], [39, 'LMH6646MM/NOB'], [41, '926-LMZ14202HTZ/NOPB'], [43, 'LMT87LPG'], [45, 'LM3480IM3-5.0'], [47, 'LT1761ES5-BYP#TRMPBF'], [49, '_331031271520'], [51, '742792097.0'], [53, 'FDV302P'], [55, 'ERJ3GEY0R00V'], [57, 'CRCW060310K0FKEA'], [59, '2447272.0'], [61, 'CRCW060320K0FKEA'], [63, '9330712.0'], [65, 'MC0063W060318K2'], [67, 'CR0603-FX-1003ELF'], [69, 'ERJ-P03J270V'], [71, 'CRCW06035K10FKEAC'], [73, '2447233.0'], [75, '#|#|Chip-Widerstand'], [77, '#'], [79, 'MC0063W060311K'], [81, 'CRCW060339K0FKEA'], [83, 'TE'], [85, 'MCWR06X3901FTL'], [87, 'WR06X3900FTL'], [89, '3.0|1K|3223W|R32, R41, RV3|'], [91, '9330941.0'], [93, '1577628.0'], [95, '3.0|1K|3223W|RV1, RV2|Bourn'], [97, 'BZX384-C16,115'], [99, 'A6S1102H'], [101, 'TP_SMD'], [103, 'S3B-ZR_THT']]
 
 
-    remove_invalid_mpns(mpns)
-
+    # remove_invalid_mpns(mpns)
+    #
     original_excel_file = '/Users/swenkoller/Desktop/GPT3 Makeathon/Code/data/24.01.2019_GZ 4136 1200b _(_vi_BOM_CSV_Komma).xlsx'
-    append_excel_with_mpns_get_csv(original_excel_file,mpns)
+    # append_excel_with_mpns_get_csv(original_excel_file,mpns)
 
+    get_result_csv(original_excel_file,mpns)
 
 
     # print("mpn_blank_ratio")
