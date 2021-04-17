0b39ea2e6eb9911f1e69b09633200aab156ef940
Swen Koller <swenkoller@Swens-MacBook-Pro.local>
2021-04-17 20:37:41 +0200

    results v2

--- results.py
+++ results.py
@@ -33,15 +33,24 @@ def number_of_mpns(mpn_candidates):
 def mpn_blank_ratio(mpn_candidates):
     return number_of_mpns(mpn_candidates) / len(mpn_candidates)
 
-def valid_mpn_ratio(mpn_candidates):
+def valid_mpns(mpn_candidates):
     valid = 0
     for row in mpn_candidates:
         if len(row) > 1:
             if match_single_mpn(row[1]) != None:
                 valid+=1
+    return valid
 
-    total = number_of_mpns(mpn_candidates)
-    return valid / total
+def valid_mpn_ratio_overall(mpn_candidates):
+    valid = valid_mpns(mpn_candidates)
+    lines = len(mpn_candidates)
+    return valid / lines
+
+
+def valid_mpn_ratio(mpn_candidates):
+    valid = valid_mpns(mpn_candidates)
+    total_candidates = number_of_mpns(mpn_candidates)
+    return valid / total_candidates
 
 """
 # input: original excel file + array excel_indices, mpn_candidates
@@ -51,12 +60,10 @@ removes invalid mpns
 """
 def append_excel_with_mpns_get_csv(original_excel,mpn_candidates):
 
-    remove_invalid_mpns(mpn_candidates)
-
     wb = xlrd.open_workbook(original_excel)
     sh = wb.sheet_by_index(0)
-    your_csv_file = open(original_excel.split('.xls')[0] + ".txt", 'w')
-    wr = csv.writer(your_csv_file, delimiter=';') #, quoting=csv.QUOTE_ALL
+    your_csv_file = open(original_excel.split('.xls')[0] + ".csv", 'w')
+    wr = csv.writer(your_csv_file, delimiter=',', quoting=csv.QUOTE_ALL)
 
     # max_width = sh.computed_column_width()
     for rownum in range(sh.nrows):
@@ -71,28 +78,52 @@ def append_excel_with_mpns_get_csv(original_excel,mpn_candidates):
             wr.writerow(sh.row_values(rownum))
 
     your_csv_file.close()
-    return new_excel_file
+    return
 
 """
 takes list as input and deletes all invalid MPNs
 """
 def remove_invalid_mpns(mpn_candidates):
+    index = 0
+
     for candidate in mpn_candidates:
+        print("")
+        index=+1
+        print(str(index))
         if len(candidate) > 1:
-            if match_single_mpn(candidate[1]) is None:
+            match = match_single_mpn(candidate[1])
+            if match is None:
+                print(candidate[1] + " is NOT a valid MPN")
                 del candidate[1]
+            # else:
+            #     print(match)
         else:
             continue
+
+
 
 # count = get_number_of_mpns([[0],
 # [1,"FCA234234NL"],
 # [2,"FLACA2334NL"]])
 if __name__ == '__main__':
-    mpns = [[0],
-            [1,"FCA234234NL"],
-            [2,"FLACA2334NL"],
-            [3,"LMC6482IMX/NOPB"]]
-    original_excel_file = '/Users/swenkoller/Desktop/GPT3 Makeathon/Code/data/Bill of Materials-D-Muster_Pyramid _V1(Standard).xlsx'
+    mpns = [[1, 'Qty.|Value|Package|Parts|Producer|Producer Number|Description|Dist'], [3, 'C0603C105K3RACTU'], [5, 'GRM188R72A104KA35D'], [7, 'MC0603B102K500CT'], [9, '0603B103J500CT'], [11, 'C1608X5R1E106M080AC'], [13, '0603B103J500CT'], [15, '#|#|'], [17, 'C9'], [19, '0603B472K500CT'], [21, 'GRM31CR71A226ME15L'], [23, ''], [25, 'C1206C475K5P'], [27, 'C3216X7T2E224M160AA'], [29, 'CD0603_S01575'], [31, 'SMAJ18CA'], [33, '634-SI8261BAC-C-IS'], [35, 'CD4093BPWR'], [37, ''], [39, 'LMH6646MM/NOB'], [41, '926-LMZ14202HTZ/NOPB'], [43, 'LMT87LPG'], [45, 'LM3480IM3-5.0'], [47, 'LT1761ES5-BYP#TRMPBF'], [49, '_331031271520'], [51, '742792097.0'], [53, 'FDV302P'], [55, 'ERJ3GEY0R00V'], [57, 'CRCW060310K0FKEA'], [59, '2447272.0'], [61, 'CRCW060320K0FKEA'], [63, '9330712.0'], [65, 'MC0063W060318K2'], [67, 'CR0603-FX-1003ELF'], [69, 'ERJ-P03J270V'], [71, 'CRCW06035K10FKEAC'], [73, '2447233.0'], [75, '#|#|Chip-Widerstand'], [77, '#'], [79, 'MC0063W060311K'], [81, 'CRCW060339K0FKEA'], [83, 'TE'], [85, 'MCWR06X3901FTL'], [87, 'WR06X3900FTL'], [89, '3.0|1K|3223W|R32, R41, RV3|'], [91, '9330941.0'], [93, '1577628.0'], [95, '3.0|1K|3223W|RV1, RV2|Bourn'], [97, 'BZX384-C16,115'], [99, 'A6S1102H'], [101, 'TP_SMD'], [103, 'S3B-ZR_THT']]
+
+
+    remove_invalid_mpns(mpns)
+    # print("mpn_blank_ratio")
+    # print(mpn_blank_ratio(mpns))
+    #
+    # print("valid_mpn_ratio")
+    # print(valid_mpn_ratio(mpns))
+    #
+    # print("valid_mpn_ratio_overall")
+    # print(valid_mpn_ratio_overall(mpns))
+
+    # mpns = [[0],
+    #         [1,"FCA234234NL"],
+    #         [2,"FLACA2334NL"],
+    #         [3,"LMC6482IMX/NOPB"]]
+    original_excel_file = '/Users/swenkoller/Desktop/GPT3 Makeathon/Code/data/24.01.2019_GZ 4136 1200b _(_vi_BOM_CSV_Komma).xlsx'
     append_excel_with_mpns_get_csv(original_excel_file,mpns)
     # count = get_number_of_mpns([[0],
     # [1,"FCA234234NL"],
